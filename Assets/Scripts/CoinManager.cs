6a94b3f0383d38fd5a2615582ef970c7e84b6f4a
kestrel <koko587066@gmail.com>
2025-03-22 15:24:27 +0900

    Coin Manager

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,30 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
-
+    [SerializeField]
+    int coinNum;
 
     public float speed = 5f;
     private Rigidbody2D rigid;
-    float currentTIme;
-    public float createTime = 1f;
-    public GameObject CoinFactory;
+
+    [SerializeField]
+    float createTime = 1f;
+
+    [SerializeField]
+    GameObject coin;
+
+    [SerializeField]
+    GameObject[] coinArray;
+
+    public GameObject[] CoinArray { get => coinArray; }
+
+    Coroutine createCoinCoroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-     
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        currentTIme += Time.deltaTime;
-        if (currentTIme > createTime)
+        GameObject nowCoin;
+        coinArray = new GameObject[coinNum];
+
+        for (int i = 0; i < coinNum; i++)
         {
-            GameObject Coin = Instantiate(CoinFactory);
-            Coin.transform.position = transform.position;
-            currentTIme = 0;
+            nowCoin = Instantiate(coin);
+            nowCoin.transform.parent = transform;
+            nowCoin.SetActive(false);
+
+            coinArray[i] = nowCoin;
         }
+
+        createCoinCoroutine = StartCoroutine(CreateCoin());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(createCoinCoroutine);
+    }
+
+    IEnumerator CreateCoin()
+    {
+        yield return new WaitForSeconds(createTime);
+
+        foreach (GameObject c in coinArray)
+        {
+            if (!c.activeSelf)
+            {
+                c.SetActive(true);
+                break;
+            }
+        }
+
+        createCoinCoroutine = StartCoroutine(CreateCoin());
+
     }
 }
