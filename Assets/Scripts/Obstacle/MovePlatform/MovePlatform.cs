0776fc04733adcf4e727b28a28aefa52d1c25856
yordleeya <lwjin9633@naver.com>
2025-04-01 15:23:33 +0900

    움직이는 장애물

---
+++ Assets/Scripts/Obstacle/MovePlatform/MovePlatform.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    [SerializeField] private Transform[] pointArray;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] Vector2[] pointPositionArray;
+    private bool isMovingToFirst = true;  // 첫 번째 포인트로 이동 중인지 확인하는 변수
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pointArray = new Transform[transform.childCount+1];
+        pointPositionArray = new Vector2[pointArray.Length];
+
+        for (int i= 0;i<pointArray.Length;i++)
+        {
+            pointArray[i] = transform.GetChild(0).GetChild(i);
+        }
+
+        int index = 0;
+
+        foreach (var point in pointArray)
+        {
+            pointPositionArray[index] = point.position;
+            point.gameObject.SetActive(false);
+            index++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingToFirst)
+        {
+            if (transform.position != (Vector3)pointPositionArray[0])
+            {
+                transform.position = Vector2.MoveTowards(transform.position, pointPositionArray[0], moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isMovingToFirst = false;
+            }
+        }
+        else
+        {
+            if (transform.position != (Vector3)pointPositionArray[1])
+            {
+                transform.position = Vector2.MoveTowards(transform.position, pointPositionArray[1], moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isMovingToFirst = true;
+            }
+        }
+    }
+}
