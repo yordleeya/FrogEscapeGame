251345c9d02a396ad5b1ddb365f505d401a94eb3
kestrel <koko587066@gmail.com>
2025-05-05 04:15:52 +0900

    씬 관련 오류 수정

--- Assets/Scripts/System/RhythmManager.cs
+++ Assets/Scripts/System/RhythmManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
+using System.Threading;
 
 public class RhythmManager : MonoBehaviour
 {
@@ -20,37 +21,49 @@ public class RhythmManager : MonoBehaviour
     public static bool IsOnBeat => isOnBeat;
 
     private bool isRunning = false;
+    private CancellationTokenSource cts;
 
     private void OnEnable()
     {
+        cts = new CancellationTokenSource();
         isRunning = true;
-        StartRhythmLoop().Forget();  // UniTask를 fire-and-forget 형태로 실행
+        StartRhythmLoop(cts.Token).Forget();
     }
 
     private void OnDisable()
     {
         isRunning = false;
+        cts?.Cancel();
+        cts?.Dispose();
+        cts = null;
     }
 
-    private async UniTaskVoid StartRhythmLoop()
+    private async UniTaskVoid StartRhythmLoop(CancellationToken token)
     {
         float interval = 60f / bpm;
 
-        while (isRunning)
+        while (isRunning && !token.IsCancellationRequested)
         {
             isOnBeat = true;
             OnBeatEnter?.Invoke();
 
-            _ = OffsetDelay(); // offset 이후에 beat 종료 알림
+            await OffsetDelay(token); // 반드시 실행되게 await로 연결
 
-            await UniTask.Delay(TimeSpan.FromSeconds(interval), ignoreTimeScale: true);
+            await UniTask.Delay(TimeSpan.FromSeconds(interval), ignoreTimeScale: true, cancellationToken: token);
         }
     }
 
-    private async UniTaskVoid OffsetDelay()
+    private async UniTask OffsetDelay(CancellationToken token)
     {
-        await UniTask.Delay(TimeSpan.FromSeconds(offset), ignoreTimeScale: true);
-        isOnBeat = false;
-        OnBeatExit?.Invoke();
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(offset), ignoreTimeScale: true, cancellationToken: token);
+            isOnBeat = false;
+            OnBeatExit?.Invoke();
+        }
+        catch (OperationCanceledException)
+        {
+            // 무시: 해제 중 취소될 수 있음
+        }
     }
 }
