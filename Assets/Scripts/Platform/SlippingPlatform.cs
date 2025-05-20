564b9b303e3ec96396083d4dbd827b148da42690
kestrel <koko587066@gmail.com>
2025-05-20 13:40:05 +0900

    slipping platform init

--- Assets/Scripts/Platform/SlippingPlatform.cs
+++ Assets/Scripts/Platform/SlippingPlatform.cs
@@ -1,13 +1,26 @@
-// c:\Users\lwjin\OneDrive\바탕 화면\capstone\Assets\Scripts\Platform\SlippingPlatform.cs
 using UnityEngine;
 
 public class SlippingPlatform : MonoBehaviour
 {
-    [Tooltip("혀가 이 플랫폼에 붙었을 때 아래로 미끄러지는 속도")]
-    public float slipSpeed = 0.5f;
+    [SerializeField]
+    RopeAction rope;
 
-    [Tooltip("혀가 최대로 미끄러질 수 있는 거리 (이 거리 도달 시 로프 해제)")]
-    public float maxSlipDistance = 0.1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Tongue"))
+        {
+            Rigidbody2D rigid = collision.GetComponent<Rigidbody2D>();
 
-    // 특별한 로직은 필요 없음. RopeAction에서 이 컴포넌트와 값들을 참조함.
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tongue") && rope.IsAttached)
+        {
+            rope.Released();
+        }
+
+    }
 }
