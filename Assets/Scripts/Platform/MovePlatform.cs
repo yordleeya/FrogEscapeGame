f26e0b8339a0a869e2bfc1fd371cf6c4c500fcf9
yordleeya <lwjin9633@naver.com>
2025-05-22 17:58:15 +0900

    움직이는 플랫폼 완성

--- Assets/Scripts/Platform/MovePlatform.cs
+++ Assets/Scripts/Platform/MovePlatform.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform point1;
+    public Transform point2;
+    public float speed = 2f;
+
+    private Vector3 point1Pos;
+    private Vector3 point2Pos;
+    private Vector3 target;
+
     void Start()
     {
-        
+        // point1, point2의 월드 좌표를 저장
+        point1Pos = point1.position;
+        point2Pos = point2.position;
+        target = point2Pos;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 저장된 월드 좌표로 이동
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.01f)
+        {
+            target = (target == point1Pos) ? point2Pos : point1Pos;
+        }
     }
 }
