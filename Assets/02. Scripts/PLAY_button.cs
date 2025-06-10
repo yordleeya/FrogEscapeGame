266670491912c7a17ef1a1a3629927dc5a6ae732
kestrel <koko587066@gmail.com>
2025-06-10 15:19:58 +0900

    영상 연결

--- Assets/02. Scripts/PLAY_button.cs
+++ Assets/02. Scripts/PLAY_button.cs
@@ -6,7 +6,7 @@ public class PLAY_button : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartButton()
     {
-        SceneManager.LoadScene("PlayScene");
+        SceneManager.LoadScene("Prologue");
     }
 
     public void Enable(UnityEngine.Video.VideoPlayer video)
